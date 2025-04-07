c8ffa8db815498cfe36a4d73953a08a70be8d6bb
franckzhuang <franck.zhuang@gmail.com>
2025-04-08 01:16:35 +0200

    feat(scrapers): Add multiprocess while scraping

--- scraper/src/main.py
+++ scraper/src/main.py
@@ -1,36 +1,93 @@
 import logging
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
-from models import PinterestImagesScraper, LummiScraper, ImpossibleImagesScraper
-from utils import download_images
+from dotenv import load_dotenv
+
+from models import (
+    PinterestImagesScraper,
+    LummiScraper,
+    ImpossibleImagesScraper,
+    UnsplashImagesScraper,
+)
+from models import Scraper
+from utils import ImageDownloader
+
+load_dotenv()
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 
-def boolean_parser(value: str) -> bool:
+def boolean_parser(value: [str, bool]) -> bool:
+    if isinstance(value, bool):
+        return value
     return value.lower() in ["true", "1"]
+
+
+def get_scraper_enabled(env_var: str) -> bool:
+    env_value = os.environ.get(env_var, "false")
+    try:
+        return boolean_parser(env_value)
+    except ValueError as e:
+        logging.warning(
+            f"Invalid value for {env_var}: {env_value}. Expected 'true' or 'false'."
+        )
+        return False
 
 
 def init_scrapers():
     scrapers = []
-    if boolean_parser(os.environ.get("LUMMMI_SCRAPER", False)) is True:
-        scrapers.append(LummiScraper())
 
-    if boolean_parser(os.environ.get("PINTEREST_SCRAPER", False)) is True:
-        scrapers.append(PinterestImagesScraper())
+    scraper_classes = [
+        (LummiScraper, "LUMMMI_SCRAPER"),
+        (PinterestImagesScraper, "PINTEREST_SCRAPER"),
+        (ImpossibleImagesScraper, "IMPOSSIBLE_IMAGES_SCRAPER"),
+        (UnsplashImagesScraper, "UNSPLASH_SCRAPER"),
+    ]
 
-    if boolean_parser(os.environ.get("IMPOSSIBLE_IMAGES_SCRAPER", False)) is True:
-        scrapers.append(ImpossibleImagesScraper())
+    for scraper_class, env_var in scraper_classes:
+        try:
+            if get_scraper_enabled(env_var):
+                logging.info(f"Initializing scraper: {scraper_class.__name__}")
+                scrapers.append(scraper_class())
+            else:
+                logging.info(
+                    f"Scraper {scraper_class.__name__} is disabled (via {env_var} environment variable)."
+                )
+        except Exception as e:
+            logging.error(
+                f"Failed to initialize {scraper_class.__name__} due to error: {e}"
+            )
+
     return scrapers
+
+
+def get_scraping_results(scrapers: list[Scraper], param: str):
+    img_links_by_site = []
+    with ThreadPoolExecutor() as executor:
+        future_to_scraper = {
+            executor.submit(scraper.get_img_links, param): scraper
+            for scraper in scrapers
+        }
+        for future in as_completed(future_to_scraper):
+            scraper = future_to_scraper[future]
+            try:
+                urls, website_name = future.result()
+                logging.info(f"{website_name}: Found {len(urls)} images.")
+                img_links_by_site.append((urls, website_name))
+            except Exception as e:
+                logging.error(f"Scraping failed for {scraper.__class__.__name__}: {e}")
+    return img_links_by_site
 
 
 def main():
     scrapers = init_scrapers()
     if not scrapers:
         logging.error(
-            "No scrapers enabled. Please set the environment variable for at least one scraper."
+            "No scrapers enabled. "
+            "Please set the environment variable for at least one scraper."
         )
         return
 
@@ -40,15 +97,9 @@ def main():
         return
     param = search.replace(" ", "%20")
 
-    for scraper in scrapers:
-        logging.info(
-            f"Running scraper - {scraper.__class__.__name__} - for search '{search}'."
-        )
-        urls, website_name = scraper.get_img_links(param, 60)
-        len_downloaded_images = download_images(urls, website_name, search)
-        logging.info(
-            f"Downloaded {len_downloaded_images}/{len(urls)} images from {website_name}."
-        )
+    img_links_by_site = get_scraping_results(scrapers, param)
+    ImageDownloader.download_all_images(img_links_by_site, search)
+    logging.info("All images downloaded successfully.")
 
 
 if __name__ == "__main__":
--- scraper/src/models.py
+++ scraper/src/models.py
@@ -1,20 +1,24 @@
+import math
 import os
+import sys
 import time
 import logging
+
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 from abc import ABC, abstractmethod
+from tqdm import tqdm
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-SCROLL_TIMEOUT = os.environ.get("SCROLL_TIMEOUT", 30)
+SCROLL_TIMEOUT = int(os.environ.get("SCROLL_TIMEOUT", 30))
 
 
 class Scraper(ABC):
     @abstractmethod
-    def get_img_links(self, search: str, scroll_timeout=0):
+    def get_img_links(self, search: str, scroll_timeout: int = 0):
         pass
 
 
@@ -24,10 +28,12 @@ class LummiScraper(Scraper):
             "https://www.lummi.ai/s/photo/"  # can use /illustration instead of /photo
         )
 
-    def get_img_links(self, search: str, scroll_timeout=0) -> tuple[list[str], str]:
+    def get_img_links(
+        self, search: str, scroll_timeout: int = 0
+    ) -> tuple[list[str], str]:
         with sync_playwright() as pw:
             browser = pw.chromium.launch(headless=True)
-            context = browser.new_context(viewport={"width": 1920, "height": 1080})
+            context = browser.new_context()
             page = context.new_page()
 
             page.goto(f"{self.url}{search}")
@@ -58,7 +64,7 @@ class ImpossibleImagesScraper(Scraper):
     def get_img_links(self, search: str, scroll_timeout=0) -> tuple[list[str], str]:
         with sync_playwright() as pw:
             browser = pw.chromium.launch(headless=True)
-            context = browser.new_context(viewport={"width": 1920, "height": 1080})
+            context = browser.new_context()
             page = context.new_page()
 
             page.goto(f"{self.url}{search}{self.additional_params}")
@@ -93,7 +99,7 @@ class PinterestImagesScraper:
     ) -> tuple[list[str], str]:
         with sync_playwright() as pw:
             browser = pw.chromium.launch(headless=True)
-            context = browser.new_context(viewport={"width": 1920, "height": 1080})
+            context = browser.new_context()
             page = context.new_page()
             page.goto(f"{self.url}{search}{self.additional_params}")
             page.wait_for_timeout(3000)
@@ -128,3 +134,67 @@ class PinterestImagesScraper:
             logging.info(f"Found {len(urls)} images.")
             browser.close()
         return urls, "Pinterest"
+
+
+class UnsplashImagesScraper:
+    def __init__(self):
+        self.url = "https://unsplash.com/s/photos/"
+        self.additional_params = "?license=free"
+
+    def get_img_links(
+        self, search: str, scroll_timeout: int = 30
+    ) -> tuple[list[str], str]:
+        with sync_playwright() as pw:
+            browser = pw.chromium.launch(headless=True)
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto(f"{self.url}{search}{self.additional_params}")
+            page.wait_for_timeout(2000)
+            logging.info("Page loaded successfully.")
+
+            urls = []
+            start_time = time.time()
+            previous_elapsed = 0
+
+            progress_bar = tqdm(
+                total=scroll_timeout,
+                desc="Scrolling",
+                ncols=100,
+                unit="sec",
+                file=sys.stdout,
+                dynamic_ncols=True,
+            )
+
+            while True:
+                page.evaluate("window.scrollBy(0, 2000)")
+                time.sleep(1)
+                html = BeautifulSoup(page.content(), "html.parser")
+
+                images = html.find_all("img")
+                image_urls = [img["src"] for img in images if "src" in img.attrs]
+
+                cleaned_urls = [
+                    url.replace("&q=60&w=3000", "")
+                    for url in image_urls
+                    if "photo" in url
+                ]
+                urls.extend([url for url in cleaned_urls if url not in urls])
+
+                elapsed = time.time() - start_time
+                increment = elapsed - previous_elapsed
+                progress_bar.update(
+                    math.ceil(min(increment, scroll_timeout - progress_bar.n))
+                )  # avoid overfill
+                previous_elapsed = elapsed
+
+                if elapsed >= scroll_timeout:
+                    progress_bar.close()
+                    logging.info(
+                        f"Timeout reached ({scroll_timeout} seconds), stopping scroll."
+                    )
+                    break
+
+            logging.info(f"Found {len(urls)} images.")
+            browser.close()
+
+        return urls, "Unsplash"
