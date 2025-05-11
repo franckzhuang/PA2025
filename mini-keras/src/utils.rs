3405460131e6051403b418979b981fda0407b684
Franck Zhuang <128403967+franckzhuang@users.noreply.github.com>
2025-05-11 18:28:46 +0200

    feat(mini-keras): Implement python binding for LinearModel (#15)

--- mini-keras/src/utils.rs
+++ mini-keras/src/utils.rs
@@ -1,6 +1,42 @@
-pub fn mse(predictions: Vec<f64>, actual: Vec<f64>) -> f64 {
+// #[derive(Debug, Clone, Copy, PartialEq)]
+// pub enum ModelMode {
+//     Regression,
+//     Classification,
+// }
+//
+// impl ModelMode {
+//     pub fn from_str(mode_str: &str) -> Result<Self, String> {
+//         match mode_str.to_lowercase().as_str() {
+//             "regression" => Ok(ModelMode::Regression),
+//             "classification" => Ok(ModelMode::Classification),
+//             _ => Err(format!(
+//                 "Invalid model mode: '{}'. Please, make sure to use 'regression' or 'classification'.",
+//                 mode_str
+//             )),
+//         }
+//     }
+// }
+
+pub fn check_mode(mode_input: &str) -> Result<String, String> {
+    let mode_lowercase = mode_input.to_lowercase();
+    if mode_lowercase == "classification" || mode_lowercase == "regression" {
+        Ok(mode_lowercase)
+    } else {
+        Err(format!(
+            "Invalid mode: '{}'. Supported modes are 'classification' or 'regression'.",
+            mode_input
+        ))
+    }
+}
+
+pub fn mse(predictions: &[f64], actual: &[f64]) -> f64 {
     if predictions.len() != actual.len() {
-        panic!("Error: predictions and actual values must have the same length");
+        panic!(
+            "Error: predictions and actual values must have the same length. \
+            Pred len: {}, Actual len: {}",
+            predictions.len(),
+            actual.len()
+        );
     }
 
     if predictions.is_empty() {
@@ -16,9 +52,14 @@ pub fn mse(predictions: Vec<f64>, actual: Vec<f64>) -> f64 {
     sum_squared_error / predictions.len() as f64
 }
 
-pub fn accuracy(predictions: Vec<f64>, actual: Vec<f64>) -> f64 {
+pub fn accuracy(predictions: &[f64], actual: &[f64]) -> f64 {
     if predictions.len() != actual.len() {
-        panic!("Error: predictions and actual values must have the same length");
+        panic!(
+            "Error: predictions and actual values must have the same length. \
+            Pred len: {}, Actual len: {}",
+            predictions.len(),
+            actual.len()
+        );
     }
 
     if predictions.is_empty() {
@@ -27,7 +68,7 @@ pub fn accuracy(predictions: Vec<f64>, actual: Vec<f64>) -> f64 {
 
     let mut correct_count = 0;
     for (pred, act) in predictions.iter().zip(actual.iter()) {
-        if pred * act > 0.0 {
+        if (*pred >= 0.0 && *act >= 0.0) || (*pred < 0.0 && *act < 0.0) {
             correct_count += 1;
         }
     }
