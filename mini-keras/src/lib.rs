3db54f9075d6a0efbbf6ab352938f8edb95a7ad8
Franck Zhuang <128403967+franckzhuang@users.noreply.github.com>
2025-06-02 01:27:19 +0200

    feat(linear-model): Add Rosenblatt and one step linear regression (#21)

--- mini-keras/src/lib.rs
+++ mini-keras/src/lib.rs
@@ -3,26 +3,91 @@
 use pyo3::prelude::*;
 use pyo3::types::PyModule;
 use pyo3::exceptions::PyValueError;
+use numpy::{PyArray1, PyArray2};
 
-mod linear_model;
+mod linear_model_gradient;
 mod mlp;
 mod utils;
+mod linear_model;
 
-use crate::linear_model::LinearModel as RustLinearModel;
+use crate::linear_model_gradient::LinearModelGradientDescent as RustLinearModelGradient;
+use crate::linear_model::LinearRegression as RustLinearRegression;
+use crate::linear_model::LinearClassification as RustLinearClassification;
+
+
 // use crate::mlp::Prediction as RustPrediction;
 // use crate::mlp::Prediction;
 use crate::mlp::Perceptron as RustPerceptron;
 use crate::mlp::DenseLayer as RustDenseLayer;
 use crate::mlp::MLP as RustMLP;
 
-#[pyclass(name = "LinearModel")]
-struct PyLinearModel {
-    model: RustLinearModel,
+#[pyclass(name = "LinearRegression")]
+struct PyLinearRegression {
+    model: RustLinearRegression,
+}
+
+#[pyclass(name = "LinearClassification")]
+struct PyLinearClassification {
+    model: RustLinearClassification,
+}
+
+#[pyclass(name = "LinearModelGradient")]
+struct PyLinearModelGradient {
+    model: RustLinearModelGradient,
 }
 
 #[pyclass(name = "MLP")]
 struct PyMLP {
     model: RustMLP,
+}
+
+#[pymethods]
+impl PyLinearRegression {
+    #[new]
+    fn new() -> PyResult<Self> {
+        let rust_model = RustLinearRegression::new();
+        Ok(PyLinearRegression { model: rust_model })
+    }
+
+    fn fit<'py>(&mut self, py: Python<'py>, x_train: &'py PyArray2<f64>, y_train: &'py PyArray1<f64>) -> PyResult<()> {
+        let x = x_train.readonly().as_array().to_owned();
+        let y = y_train.readonly().as_array().to_owned();
+        self.model.fit(&x, &y);
+        Ok(())
+    }
+
+    fn predict<'py>(&self, py: Python<'py>, x: &'py PyArray2<f64>) -> PyResult<&'py PyArray1<f64>> {
+        let x = x.readonly().as_array().to_owned();
+        let result = self.model.predict(&x);
+        Ok(PyArray1::from_owned_array(py, result))
+    }
+
+}
+
+#[pymethods]
+impl PyLinearClassification {
+    #[new]
+    fn new(learning_rate: Option<f64>, max_iterations: Option<usize>, verbose: Option<bool>) -> PyResult<Self> {
+        let rust_model = RustLinearClassification::new(learning_rate, max_iterations, verbose);
+        Ok(PyLinearClassification { model: rust_model })
+    }
+
+    fn fit(&mut self, x_train: Vec<Vec<f64>>, y_train: Vec<f64>) -> PyResult<()> {
+        self.model.fit(&x_train, &y_train);
+        Ok(())
+    }
+
+    fn predict(&self, x: Vec<f64>) -> PyResult<f64> {
+        Ok(self.model.predict(&x))
+    }
+    
+    fn get_weights(&self) -> PyResult<Vec<f64>> {
+        Ok(self.model.get_weights())
+    }
+    
+    fn get_bias(&self) -> PyResult<f64> {
+        Ok(self.model.get_bias())
+    }   
 }
 
 #[pymethods]
@@ -53,14 +118,14 @@ impl PyMLP {
 }
 
 #[pymethods]
-impl PyLinearModel {
+impl PyLinearModelGradient {
     #[new]
     fn new(learning_rate: f64, epochs: usize, mode: String, verbose: bool) -> PyResult<Self> {
         let rust_model =
-            RustLinearModel::new(learning_rate, epochs, mode, verbose)
+            RustLinearModelGradient::new(learning_rate, epochs, mode, verbose)
                 .map_err(|e_str| PyValueError::new_err(e_str))?;
 
-        Ok(PyLinearModel { model: rust_model })
+        Ok(PyLinearModelGradient { model: rust_model })
     }
 
     fn fit(&mut self, x_train: Vec<Vec<f64>>, y_train: Vec<f64>) -> PyResult<()> {
@@ -75,7 +140,9 @@ impl PyLinearModel {
 
 #[pymodule]
 fn mini_keras(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<PyLinearModel>()?;
+    m.add_class::<PyLinearModelGradient>()?;
     m.add_class::<PyMLP>()?;
+    m.add_class::<PyLinearRegression>()?;
+    m.add_class::<PyLinearClassification>()?;
     Ok(())
 }
--- mini-keras/src/linear_model.rs
+++ mini-keras/src/linear_model.rs
@@ -1,113 +1,135 @@
-use crate::utils;
+use ndarray::{Array1, Array2, s};
+use ndarray::linalg::Dot;
+use ndarray_linalg::Inverse;
 
-pub struct LinearModel {
+
+pub struct LinearClassification {
     pub weights: Vec<f64>,
-    pub bias: f64,
     learning_rate: f64,
-    epochs: usize,
-    mode: String,
+    max_iterations: usize,
     verbose: bool,
 }
 
-impl LinearModel {
-    pub fn new(learning_rate: f64, epochs: usize, mode: String, verbose: bool) -> Result<Self, String>  {
-        let normalized_mode = utils::check_mode(&mode)?;
-        Ok(Self {
+impl LinearClassification {
+    pub fn new(learning_rate: Option<f64>, max_iterations: Option<usize>, verbose: Option<bool>) -> Self {
+        Self {
             weights: Vec::new(),
-            bias: 0.0,
-            learning_rate,
-            epochs,
-            mode: normalized_mode,
-            verbose,
-        })
+            learning_rate: learning_rate.unwrap_or(0.01),
+            max_iterations: max_iterations.unwrap_or(1000),
+            verbose: verbose.unwrap_or(false),
+        }
+    }
+    fn add_bias(x: &[f64]) -> Vec<f64> {
+        let mut v = Vec::with_capacity(x.len() + 1);
+        v.push(1.0);
+        v.extend(x.iter().cloned());
+        v
     }
 
-    fn linear_output(&self, x: &[f64]) -> f64 {
-        let mut linear_output = self.bias;
-        for (i, &weight) in self.weights.iter().enumerate() {
-            linear_output += weight * x[i];
-        }
-        linear_output
+    fn predict_fit(&self, x_aug: &[f64]) -> f64 {
+        let activation: f64 = x_aug.iter()
+            .zip(self.weights.iter())
+            .map(|(x, w)| w * x)
+            .sum();
+        if activation > 0.0 { 1.0 } else { -1.0 }
     }
 
-    pub fn predict(&self, x: &[Vec<f64>]) -> Vec<f64> {
-        let mut y_preds: Vec<f64> = Vec::with_capacity(x.len());
-
-        for x_i in x {
-            let pred = self.linear_output(x_i.as_slice());
-            if self.mode == "classification".to_string() {
-                y_preds.push(if pred >= 0.0 { 1.0 } else { -1.0 });
-            } else {
-                y_preds.push(pred);
-            }
-        }
-        y_preds
+    pub fn predict(&self, x: &[f64]) -> f64 {
+        let x_aug = Self::add_bias(x);
+        self.predict_fit(&x_aug)
     }
 
-    fn gradient_descent(&self, x: &[Vec<f64>], y: &[f64]) -> (Vec<f64>, f64) {
-        let m = x.len();
-        let n = if m > 0 { x[0].len() } else { self.weights.len() };
-        let mut dw = vec![0.0; n];
-        let mut db = 0.0;
-
-        for (x_i, &y_true) in x.iter().zip(y.iter()) {
-            let y_pred = self.linear_output(x_i.as_slice());
-            let error = if self.mode == "classification".to_string() {
-                (if y_pred >= 0.0 { 1.0 } else { -1.0 }) - y_true
-            } else {
-                y_pred - y_true
-            };
-
-            for j in 0..n {
-                dw[j] += 2.0 * x_i[j] * error;
-            }
-            db += 2.0 * error;
+    pub fn fit(&mut self, x_data: &[Vec<f64>], y_data: &[f64]) {
+        if x_data.len() != y_data.len() {
+            panic!("Error: x and y must have the same length.");
         }
 
-        if m > 0 {
-            for i in 0..n {
-                dw[i] /= m as f64;
-            }
-            db /= m as f64;
-        }
+        let x_data_bias: Vec<Vec<f64>> = x_data.iter()
+            .map(|x_i| Self::add_bias(x_i))
+            .collect();
 
-        (dw, db)
-    }
+        let n = x_data_bias[0].len();
+        self.weights = vec![0.0; n];
 
-    pub fn fit(&mut self, x_train: &[Vec<f64>], y_train: &[f64]) {
-        if x_train.len() != y_train.len() {
-            panic!("Error: x_data and y_data must have the same length.");
-        }
+        let mut errors= 0;
 
-        let n = if !x_train.is_empty() { x_train[0].len() } else { 0 };
-
-        if self.weights.len() != n || (self.weights.is_empty() && n > 0) {
-            self.weights = vec![0.0; n];
-            self.bias = 0.0;
-        }
-
-        if n == 0 && !x_train.is_empty() && x_train[0].is_empty() {
-        }
-
-        for epoch in 0..self.epochs {
-            let (dw, db) = self.gradient_descent(x_train, y_train);
-
-            for (weight, &gradient) in self.weights.iter_mut().zip(dw.iter()) {
-                *weight -= self.learning_rate * gradient;
-            }
-            self.bias -= self.learning_rate * db;
-
-            if self.verbose && epoch % 100 == 0 {
-                let predictions = self.predict(x_train);
-
-                if self.mode == "classification".to_string() {
-                    let acc = utils::accuracy(&predictions, y_train);
-                    println!("Epoch {}, Accuracy: {:.2}%", epoch, acc * 100.0);
-                } else {
-                    let mse = utils::mse(&predictions, y_train);
-                    println!("Epoch {}, MSE: {:.4}", epoch, mse);
+        for iter in 0..self.max_iterations {
+            errors = 0;
+            for (x, &y) in x_data_bias.iter().zip(y_data.iter()) {
+                let y_pred = self.predict_fit(x);
+                if y_pred != y {
+                    self.weights = self.weights.iter()
+                        .zip(x.iter())
+                        .map(|(w_i, x_i)| w_i + self.learning_rate * (y - y_pred) * x_i)
+                        .collect::<Vec<f64>>();
+                    errors += 1;
                 }
             }
+            if errors == 0 {
+                if self.verbose {
+                    println!("Convergence reached after {} iterations.", iter);
+                }
+                break;
+            }
         }
+        if errors != 0 && self.verbose {
+            println!("Maximum number of iterations reached, convergence not reached.")
+        }
+    }
+    
+    pub fn get_weights(&self) -> Vec<f64> {
+        self.weights.clone()
+    }
+    
+    pub fn get_bias(&self) -> f64 {
+        if self.weights.is_empty() {
+            0.0
+        } else {
+            self.weights[0]
+        }
+    }
+}
+
+pub struct LinearRegression {
+    weights: Option<Array1<f64>>,
+    bias: Option<f64>,
+}
+
+impl LinearRegression {
+    pub fn new() -> Self {
+        Self { weights: None, bias: None }
+    }
+
+    fn augment_with_ones(x: &Array2<f64>) -> Array2<f64> {
+        let n_samples = x.nrows();
+        let n_features = x.ncols();
+
+        let mut x_aug = Array2::<f64>::ones((n_samples, n_features + 1));
+
+        for i in 0..n_samples {
+            for j in 0..n_features {
+                x_aug[(i, j + 1)] = x[(i, j)];
+            }
+        }
+        x_aug
+    }
+
+    pub fn fit(&mut self, x: &Array2<f64>, y: &Array1<f64>) {
+        let x_aug = Self::augment_with_ones(x);
+
+        let xtx = x_aug.t().dot(&x_aug);
+        let xty = x_aug.t().dot(y);
+        let xtx_inv = xtx.inv().expect("XTX not inversible !");
+        let all_weights = xtx_inv.dot(&xty);
+
+        self.bias = Some(all_weights[0]);
+        self.weights = Some(all_weights.slice(s![1..]).to_owned());
+    }
+
+    pub fn predict(&self, x: &Array2<f64>) -> Array1<f64> {
+        let weights = self.weights.as_ref().expect("Not fit");
+        let bias = self.bias.expect("Not fit");
+
+        x.dot(weights) + bias
     }
 }
